efa9cac09c1b15bf3b4b2505bbde07f069d5981d
dust <dustlike@ms.simis.com.tw>
2018-04-24 13:44:37 +0800

    first commit

---
+++ library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2018/4/18 上午 10:29:40
+ * Author : user
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
