150b702030e0441244a379b03f28cbebfc09e48c
dust <dustlike@ms.simis.com.tw>
2018-05-21 16:46:17 +0800

    修正wiring_analog.c缺乏常數定義無法編譯的問題 上次改完沒編譯就commit了 有夠雷

--- include/variants/variant.h
+++ include/variants/variant.h
@@ -67,12 +67,24 @@ extern "C"
 #define digitalPinHasPWM(P)        ( g_APinDescription[P].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P].ulTCChannel != NOT_ON_TIMER )
 
 /*
- * digitalPinToTimer(..) is AVR-specific and is not defined for SAMD
- * architecture. If you need to check if a pin supports PWM you must
- * use digitalPinHasPWM(..).
- *
- * https://github.com/arduino/Arduino/issues/1833
+ * Analog pins
  */
+#define PIN_A0               (24ul)
+#define PIN_A1               (25ul)
+#define PIN_A2               (26ul)
+#define PIN_A3               (27ul)
+#define PIN_A4               (28ul)
+#define PIN_A5               (29ul)
+#define PIN_DAC0             (24ul)
+
+static const uint8_t A0  = PIN_A0;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A3  = PIN_A3;
+static const uint8_t A4  = PIN_A4;
+static const uint8_t A5  = PIN_A5;
+static const uint8_t DAC0 = PIN_DAC0;
+#define ADC_RESOLUTION		12
 
 /*
  * USB
